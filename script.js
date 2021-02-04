2b1b887493cc21f758ade74126d24b849bf3d613
celia77940 <celiarameau@live.fr>
2021-02-04 12:00:36 +0100

    fini

---
+++ script.js
@@ -0,0 +1,24 @@
+
+const link = document.querySelector('a')
+const form = document.querySelector('form');
+
+link.addEventListener('click', function(e){
+    e.preventDefault();
+    console.log('coucou')
+    const text= document.querySelector("#text");
+    text.classList.remove('hidden');
+});
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    console.log('coucou')
+})
+
+
+
+
+
+
+
+
+
